17bf6bbdbb3647c9aea82131a5f8497745fad339
ehaijnil <cc.linjiahe@gmail.com>
2020-03-21 23:16:36 -0400

    Update _LIB_Regularized_VARd_LS.R

--- _LIB_Regularized_VARd_LS.R
+++ _LIB_Regularized_VARd_LS.R
@@ -44,15 +44,14 @@ regularized_VAR_est = function(Y=NULL,X,d=NULL,lambda,penalty.factor=NULL,alpha=
     # {$A,matrix} col-concatenated version of Alist, of size p by (p*d)
     # {$Alist,list} each element corresponding to the coef estimate of the lag
     
-	n = nrow(X); p = dim(X);
+	n = nrow(X); p = ncol(X);
     
     is_Y_provided = !is.null(Y);
     if (!is_Y_provided)
 	{
     	if (is.null(d))
-			stop("Error in sparse_VAR_est(): response matrix is not specified. d must be non NULL.\n");
+			stop("Error in regularized_VAR_est(): response matrix is not specified. d must be non NULL.\n");
 		
-		n_initial = nrow(X);
 		Y = X[(d+1):n,];
 		Xlist = list();
 		for (i in 1:d)
@@ -75,7 +74,6 @@ regularized_VAR_est = function(Y=NULL,X,d=NULL,lambda,penalty.factor=NULL,alpha=
 	if (is.null(penalty.factor))
 	{
         A = array(0,c(dimY,dimX));
-        
         if (parallel)
         {
             Arow_list = foreach(j = 1:dimY,.packages=pkgs) %dopar%
@@ -133,21 +131,20 @@ regularized_VAR_est = function(Y=NULL,X,d=NULL,lambda,penalty.factor=NULL,alpha=
             skeleton_temp = which(skeleton[j,]!=0);
             if ( length(skeleton_temp) < nrow(X) && length(skeleton_temp) > 0 )
             {
-                A[,skeleton_temp] = as.numeric(lm(Y[,j]~X[,skeleton_temp]+0)$coef);
+                A[j,skeleton_temp] = as.numeric(lm(Y[,j]~X[,skeleton_temp]+0)$coef);
             }
             else
                 next;
         }
     }
-    
-    if ( (d*p)!= ncol(A) )
-    {
-        warning("The number of columns of A does not equal to dim(X) * d.\n");
-    }
-    Alist = lapply(1:d,function(x){A[,(1+(x-1)*p):(x*p)]});
+
+	col_size = round(ncol(A)/d);
+	stopifnot(col_size*d==ncol(A));
+	
+    Alist = lapply(1:d,function(xx){A[,(1+(xx-1)*col_size):(xx*col_size)]});
     names(Alist) = paste("d=",1:d,sep="");
 	
-    return(list(A=A,Alist));
+    return(list(A=A,Alist=Alist));
 }
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
@@ -170,15 +167,14 @@ regularized_VAR_bic = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.factor=NU
     # {$idx,double} the idx of the BIC selected
     # {$output,list} the output from the final sparse VAR estimation. see documentation for sparse_VAR_est()
     
-    n = nrow(X); p = dim(X);
+    n = nrow(X); 
     
     is_Y_provided = !is.null(Y);
     if (!is_Y_provided)
     {
         if (is.null(d))
-            stop("Error in sparse_VAR_est(): response matrix is not specified. d must be non NULL.\n");
+            stop("Error in regularized_VAR_bic(): response matrix is not specified. d must be non NULL.\n");
         
-        n_initial = nrow(X);
         Y = X[(d+1):n,];
         Xlist = list();
         for (i in 1:d)
@@ -195,7 +191,7 @@ regularized_VAR_bic = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.factor=NU
         }
     }
     
-    dimX = ncol(X); dimY = ncol(Y);
+    dimX = ncol(X); 
     stopifnot( nrow(X) == nrow(Y) );
     
     if (is.null(lambda_seq))
@@ -208,7 +204,7 @@ regularized_VAR_bic = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.factor=NU
     for (k in 1:length(lambda_seq))
     {
         ## note that during selection step, refitting is disabled
-        temp = regularized_VAR_est(Y=Y,X=X,d=d,lambda=lambda_seq[k],penalty.factor=penalty.factor,alpha=alpha,refit=FALSE,parallel=parallel);
+        temp = regularized_VAR_est(Y=Y,X=X,d=NULL,lambda=lambda_seq[k],penalty.factor=penalty.factor,alpha=alpha,refit=FALSE,parallel=parallel);
         
         bic_vec[k] = BIC_calc(Y,
                               X,
@@ -223,14 +219,14 @@ regularized_VAR_bic = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.factor=NU
     out = regularized_VAR_est(Y=Y,X=X,d=d,lambda=lambda_active,penalty.factor=penalty.factor,alpha=alpha,refit=refit,parallel=parallel);
     
     ## return the idx of the BIC selected and the output from the final sparse VAR estimation
-    return(list(idx=id,out=output));
+    return(list(idx=id,out=out));
 }
 
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Regularized estimate of a VAR with auto-selected tuning parameter
 # based on 1-step-ahead rolling window validation
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-regularied_VAR_validate = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.factor=NULL,alpha=alpha,
+regularized_VAR_validate = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.factor=NULL,alpha=alpha,
         train_size=0.8,validate_size=0.1,refit=FALSE,parallel=FALSE)
 {
     # {param,matrix} Y: response vectors stacked in rows
@@ -249,15 +245,14 @@ regularied_VAR_validate = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.facto
     # {$idx,double} the idx of the tunning parameter selected
     # {$output,list} the output from the final sparse VAR estimation. see documentation for sparse_VAR_est()
 
-    n = nrow(X); p = dim(X);
+    n = nrow(X); 
     
     is_Y_provided = !is.null(Y);
     if (!is_Y_provided)
     {
         if (is.null(d))
-            stop("Error in sparse_VAR_est(): response matrix is not specified. d must be non NULL.\n");
+            stop("Error in regularized_VAR_validate(): response matrix is not specified. d must be non NULL.\n");
         
-        n_initial = nrow(X);
         Y = X[(d+1):n,];
         Xlist = list();
         for (i in 1:d)
@@ -274,7 +269,7 @@ regularied_VAR_validate = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.facto
         }
     }
     
-    dimX = ncol(X); dimY = ncol(Y); effective_n = nrow(X);
+    dimX = ncol(X); effective_n = nrow(X);
     stopifnot( nrow(X) == nrow(Y) );
     
     if (is.null(lambda_seq))
@@ -286,7 +281,7 @@ regularied_VAR_validate = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.facto
     train_size = round(effective_n*train_size);
     validate_size = round(effective_n*validate_size);
     window_number = effective_n - train_size - validate_size;
-    cat(sprintf("train size = %d, test size = %d, validation window number = %d.\n", window_number))
+    cat(sprintf("train size = %d, test size = %d, validation window number = %d.\n", train_size, validate_size, window_number))
     
     validate_err = array(0,c(length(lambda_seq),window_number));
     
@@ -297,8 +292,9 @@ regularied_VAR_validate = function(Y=NULL,X,d=NULL,lambda_seq=NULL,penalty.facto
         for (i in 1:window_number)
         {
             # estimation
-            Y_est = Y[i:(i+train_size-1),]; X_est = X[i:(i+train_size-1),];
-            temp_est = regularized_VAR_est(Y=Y,X=X,d=d,lambda=lambda_temp,penalty.factor=penalty.factor,alpha=alpha,refit=FALSE,parallel=parallel);
+            Y_est = Y[i:(i+train_size-1),]; 
+            X_est = X[i:(i+train_size-1),];
+            temp_est = regularized_VAR_est(Y=Y_est,X=X_est,d=NULL,lambda=lambda_temp,penalty.factor=penalty.factor,alpha=alpha,refit=FALSE,parallel=parallel);
             
             # validation
             Y_vld = Y[(i+train_size):(i+train_size+validate_size-1),];
