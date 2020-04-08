8e5ba14e00b4f607c2d69c6c7148fe6336533cc1
Lin, J <8700972+jhlinplus@users.noreply.github.com>
2020-04-08 18:49:24 -0400

    update comments

--- Example_FAVAR_estimation.R
+++ Example_FAVAR_estimation.R
@@ -24,35 +24,33 @@ X = read.csv("example_data/X.csv")[,-1]; ## first column records the time index,
 ################################
 ## estimate using auto FAVAR 
 ################################
+
 ### tuning parameter specification
-rk_seq = seq(from=3,to=7,by=1) ## can be NULL
-lambda_Gamma_seq = seq(from=0.25,to=3,by=0.25)*sqrt(log(ncol(X))/nrow(X)); ## can be NULL
-lambda_A_seq = NULL
+rk_seq = seq(from=3,to=7,by=1) ## tuning parameter sequence for the rank specification.
+lambda_Gamma_seq = seq(from=0.25,to=3,by=0.25)*sqrt(log(ncol(X))/nrow(X)); ## tuning parameter sequence for the penalty corresponding to Gamma, can be NULL
+lambda_A_seq = NULL ## tuning parameter for the regularized estimation for the VAR process, can be NULL (read the comments to see the default setup)
+
 ### Other specifications: 
 #>> factors are recovered using PC3, i.e., no restrictions
 #>> the VAR process is estimated using lasso (alpha=1), with the true lag specification (d=2)
 IR = "PC3";
 d = 2;
 alpha = 1;
-## run
+
+## run FAVAR, tuning parameters are automatically selected based on a lattice of tuning parameters
 out_autoFAVAR = favar_auto(Y,X,rk_seq,lambda_Gamma_seq,IR=IR,lambda_A_seq,d=d,alpha=alpha,parallel=TRUE,verbose=TRUE)
 
 ################################
-## evaluate the performance -- note that this is only valid for synthetic data
+## extracting estimated components
 ################################
+## estimated Gamma -- regression coefficient in the calibration (information) equation
+out_autoFAVAR$est_Gamma
 
-## estimated Gamma
-trueGamma = read.csv("example_data/trueGamma.csv");
-Eval_Sparse(trueGamma, out_autoFAVAR$est_Gamma)
+## estimated hyperplane, i.e., Theta-hat
+out_autoFAVAR$est_Theta
 
-## estimated hyperplane
-trueF = as.matrix(read.csv("example_data/trueF.csv")[,-1]);
-trueLambda = as.matrix(read.csv("example_data/trueLambda.csv"));
-Eval_Flats(trueF %*% t(trueLambda), out_autoFAVAR$est_Theta)$metrics
+## estimated leading transition matrix (lag=1) for the VAR process
+out_autoFAVAR$est_A[[1]]
 
-## estimated transition matrix
-trueA1 = read.csv("example_data/trueA1.csv");
-Eval_Sparse(trueA1, out_autoFAVAR$est_A[[1]])
-
-trueA2 = read.csv("example_data/trueA1.csv");
-Eval_Sparse(trueA2, out_autoFAVAR$est_A[[2]])
+## estimated transition matrix for lag = 2 for the VAR process
+out_autoFAVAR$est_A[[2]]
